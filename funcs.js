6174a928478a08b1c1f6b514f01218aa1f615e01
MadHatter228 <85356819+MadHatter228@users.noreply.github.com>
2021-12-24 05:46:19 +0200

    adding initElement function

--- funcs.js
+++ funcs.js
@@ -1,7 +1,22 @@
-function moveToCenter(HTMLelement){
-    if(typeof HTMLelement == "object"){
-        HTMLelement.style=`position:absolute; top:${(window.innerHeight / 2) - (HTMLelement.offsetHeight / 2)}px; left:${(window.innerWidth / 2) - (HTMLelement.offsetWidth / 2)}px;`;
-    }else{
-        throw new Error("Wrong argument type!");
+function moveToCenter(HTMLelement) {
+    if (typeof HTMLelement == "object") {
+        HTMLelement.style = `position:absolute; top:${(window.innerHeight / 2) - (HTMLelement.offsetHeight / 2)}px; left:${(window.innerWidth / 2) - (HTMLelement.offsetWidth / 2)}px;`;
+    } else {
+        throw new Error("Wrong argument type [HTMLelement]!");
+    }
+}
+function initElement(HTMLelement, propertys = {}, attributes = {}, classes = []) {
+    if (typeof HTMLelement == "object") {
+        for (let property in propertys) {
+            HTMLelement[property] = propertys[property];
+        }
+        for (let attribute in attributes) {
+            HTMLelement.setAttribute(attribute, attributes[attribute]);
+        }
+        for (let className in classes) {
+            HTMLelement.classList.add(classes[className]);
+        }
+    } else {
+        throw new Error("Wrong argument type [HTMLelement]!");
     }
 }
